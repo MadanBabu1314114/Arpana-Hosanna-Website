b15e69ba68b9efe2e829614e37eee8db759d6bc1
Madan-SquadSync <166619662+Madan-SquadSync@users.noreply.github.com>
2025-09-15 06:30:44 +0530

    v0.0.3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "294377819"
+    serviceWorkerVersion: "3401835728"
   }
 });
